e9c47f666a40eab925fdbec66c52b614fceff65b
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-12-03 16:50:38 +0000

    Created template

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,62 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:     You are to create an Inventory Tracker System
+//              In data/runresults.txt you will see how the tracker is to
+//              work. You will need to write the definition for each function
+//              prototype in the order as they appear below.
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-int main () {
+const char MENU_QUIT = 'Q';
+const char MENU_PRINT = 'P';
+const char MENU_BUY = 'B';
+const char MENU_TOTAL = 'T';
 
-  cout << "Hello World" << endl;
+struct Item {
+  string mProductName;
+  string mID;
+  double mPrice;
+  double mQuantity;
+};
+
+char getMenuSelection ();
+void getItem (Item& item);
+int getItems (Item items[], string fileName);
+void printItem (const Item& item);
+void printItems (const Item items[], int size);
+double getInventoryValue (const Item items[], int size);
+void buyItem (Item items[], int &size, string id, double quantity);
+
+int main () {
+  const int MAX_ITEMS = 100;
+  const string ITEM_FILE_NAME = "data/inventory.txt";
+  Item items[MAX_ITEMS];
+  int size;
+  char menuSelection;
+
+  cout << fixed << setprecision (2);
+
+  // Write the code to call getItems which will load the struct array
+  // with the inventory items from the file in ITEM_FILE_NAME
+
+
+
+  // Write the code to get a menu selection. Then using a switch, call
+  // the correct function to perform the user's selection.
+  // NOTE: When you get to the last choice which is MENU_BUY in the switch,
+  //       you will need to ask the user for the item id and quantity
+  //       before calling the buyItem function
   
+
   return EXIT_SUCCESS;
 }
